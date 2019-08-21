370fa0218e737917c8779ccf8492fa9950e785a0
Viktor Lepshko <lepvv@mail.ru>
2019-08-21 12:47:06 +0300

    test commit to git hosting

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
 git commit -m "test commit to git hosting"
-git push test_gitlab -all
-git push test_github -all
-git push test_bitbucket -all
+git push test_gitlab --all
+git push test_github --all
+git push test_bitbucket --all
